eaba5960c11a47edb519e5e34b31808536eff2b5
BrysonF17 <2bfisher17@gmail.com>
2026-04-08 16:44:14 -0400

    updates for HW6

--- API/nfl_playoffs_api.py
+++ API/nfl_playoffs_api.py
@@ -1,4 +1,4 @@
-from validate_user import validate_user
+from API.validate_user import validate_user
 from fastapi import FastAPI
 from get_teams_by_conference_division import get_teams_by_conference_division
 from get_teams_in_same_conference_division_as_specified_team import get_teams_in_same_conference_division_as_specified_team
