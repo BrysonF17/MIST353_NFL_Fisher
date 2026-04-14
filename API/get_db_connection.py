c412a5df214d3e5661669c5af344168fe2c13e6e
BrysonF17 <2bfisher17@gmail.com>
2026-04-14 12:37:15 -0400

    Made changes to YML

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -11,5 +11,5 @@ def get_db_connection():
     database = os.getenv("DB_NAME")
     username = os.getenv("DB_LOGIN")
     password = os.getenv("DB_PASSWORD")
-    connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
     return pyodbc.connect(connection_string)
